7b3d00613a390b2e792dee27481b7a7b9d7c1391
tim-salabim <tim.appelhans@gmail.com>
2014-12-11 13:56:20 +0100

    updated lut

--- src/reclass_luc_names.R
+++ src/reclass_luc_names.R
@@ -9,9 +9,10 @@ shp <- readOGR("training/ps-hm-ws_all_polygons_utm_32n_etrs89.shp",
                layer = lyr, stringsAsFactors = FALSE)
 
 
-lut <- read.csv("lookup_classes.csv", stringsAsFactors = FALSE)
+lut <- read.csv("~/software/lehre/bsc-hemeroby/src/lookup_classes.csv",
+                stringsAsFactors = FALSE)
 
-for(i in 1:nrow(lut)){
+  for(i in 1:nrow(lut)){
   value <- lut[i, 1]
   name <- lut[i, 2]
   shp@data$Classname[shp@data$Classv == value] <- name
